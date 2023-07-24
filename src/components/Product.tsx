07caf18c836cba2e497980d4e1e42a29a6e872ec
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-07-24 19:21:28 +0300

    v1.0.1

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -5,10 +5,10 @@ import Divider from './Divider';
 
 const Product = () => {
   const { product } = config;
-  const [firstItem, secondItem] = product.items;
+  const { items } = product;
 
   return (
-    <section className={`bg-background py-8`} id="product">
+    <section className={`bg-background py-8`} id="why-us">
       <div className={`container max-w-5xl mx-auto m-8`}>
         <h1
           className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
@@ -23,42 +23,42 @@ const Product = () => {
           ))}
         </h1>
         <Divider />
-        <div className={`flex flex-wrap`}>
-          <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-            <h3
-              className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-            >
-              {firstItem?.title}
-            </h3>
-            <p className={`text-gray-600`}>{firstItem?.description}</p>
-          </div>
-          <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={firstItem?.img}
-              alt={firstItem?.title}
-            />
-          </div>
-        </div>
-        <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
-          <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={secondItem?.img}
-              alt={secondItem?.title}
-            />
-          </div>
-          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-            <div className={`align-middle`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
-                {secondItem?.title}
-              </h3>
-              <p className={`text-gray-600 mb-8`}>{secondItem?.description}</p>
+        {items.map((item, index) => {
+          const { title, description, img } = item;
+          const isOdd = index % 2 === 0;
+
+          return isOdd ? (
+            <div className={`flex flex-wrap`}>
+              <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
+                <h3
+                  className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+                >
+                  {title}
+                </h3>
+                <p className={`text-gray-600`}>{description}</p>
+              </div>
+              <div className={`w-full sm:w-1/2 p-6`}>
+                <img className="h-6/6" src={img} alt={title} />
+              </div>
             </div>
-          </div>
-        </div>
+          ) : (
+            <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
+              <div className={`w-full sm:w-1/2 p-6`}>
+                <img className="h-6/6" src={img} alt={title} />
+              </div>
+              <div className={`w-full sm:w-1/2 p-6 mt-20`}>
+                <div className={`align-middle`}>
+                  <h3
+                    className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+                  >
+                    {title}
+                  </h3>
+                  <p className={`text-gray-600 mb-8`}>{description}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
